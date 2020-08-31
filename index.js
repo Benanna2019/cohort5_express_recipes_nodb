4a6f540c629af9f782be6e106bd40f6bb834f234
cawilson1 <cawilson1@g.cofc.edu>
2020-08-31 10:56:12 -0400

    add a few get routes

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const recipes = require("./recipesFakeDB");
 const express = require("express");
+const { filter } = require("ramda");
 const PORT = 4000;
 
 const app = express();
 
-// console.log(app);
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/recipes", (request, response) => {
+  try {
+    console.log("success!");
+    response.status(200).send(recipes);
+  } catch (error) {
+    console.log("failure!");
+    response.status(500).send(error);
+  }
+});
+
+app.get("/recipe", (request, response) => {
+  try {
+    console.log("success!");
+    console.log(request.query);
+    const filterer = recipe => recipe.id == request.query.id;
+    response.status(200).send(filter(filterer, recipes));
+  } catch (error) {
+    console.log("failure!");
+    response.status(500).send(error);
+  }
+});
+
+app.get("/recipes_by_time", (request, response) => {
+  try {
+    console.log("RECIPES BY TIME");
+    const filterer = recipe => recipe.cookTime <= request.query.time;
+    response.status(200).send(filter(filterer, recipes));
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
 
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
